7cd27a76221034b03d66c459f82244dd7616a283
Manish Baral <manish.baral@ebpearls.com>
2024-01-30 18:35:22 +0545

    this is the second branch

--- lua/shanks/packer.lua
+++ lua/shanks/packer.lua
@@ -52,7 +52,7 @@ use("nvim-tree/nvim-web-devicons")
     }
         use {
         'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
+        branch = 'v3.x',
         requires = {
             -- LSP Support
             { 'neovim/nvim-lspconfig' },             -- Required
